684649fd861a093dfe553ac26b05b1683ebb8414
kajal55409 <kajalgupta.it@gmail.com>
2024-01-07 11:08:02 +0530

    Added screenshots

---
+++ src/question.js
@@ -0,0 +1,48 @@
+// Interview Technical coding
+// Pune Leocoders
+
+// ### Project Overview:
+ 
+// #### Objective:
+// Develop a comprehensive mobile application to manage user attendance, allowing registered users to mark their attendance using geolocation while providing admin access to oversee user lists and details.
+ 
+// ### Features:
+ 
+// #### User Registration & Authentication:
+// 1. **Registration Form:**
+//    - Create a user registration form with fields for name, email, phone number, DOB, gender, habits, "About Me" description, and profile photo upload with validation.
+// 2. **Separate Logins:**
+//    - Implement separate login screens for admin and regular users with unique credentials.
+ 
+// #### Admin Dashboard & User Management:
+// 3. **Admin Access:**
+//    - Upon successful admin login, direct to an admin-specific dashboard.
+//    - Admins can manage user lists and details (CRUD operations: View, Edit, Delete).
+ 
+// #### User Home Screen & Attendance Marking:
+// 4. **Normal User:**
+//    - After logging in, regular users should be directed to a home screen displaying their profile details (name, email, profile photo).
+//    - Users can mark attendance using geolocation on their home screen.
+
+// #### User Profile Details:
+// 5. **Details View:**
+//    - Allow users to view and edit their profile details, including name, email, phone number, DOB, habits, "About Me" description, and profile photo.
+ 
+// ### Additional Considerations:
+
+// - **Access Control:** Implement role-based access control to restrict normal users from accessing admin-exclusive functionalities.
+// - **Form Validation:** Ensure proper validation for all input fields in the registration form and profile details.
+// - **Geolocation Usage:** Utilize React Native's Geolocation API for attendance marking and fetching user locations.
+// - **User Interface & Responsiveness:** Create a user-friendly interface that works seamlessly across different devices and screen sizes.
+ 
+// ### Evaluation and Submission:
+ 
+// - Evaluate the complete functionality of user registration, logins, admin dashboard features, attendance marking, and user profile management.
+// - Candidates should submit the entire source code with setup instructions and any relevant notes on a version control platform like GitHub or Bitbucket.
+// Registration form is done with validation
+// login page is done with validation (left with logics to bring separate home)
+// Admin Access: not done
+// **Normal User:** - user can see the name and email
+// #### User Profile Details: Not completed (left with fetching the data from async storage)
+
+ 
